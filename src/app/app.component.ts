8a60e171df89e118fac852c7493d3b692fe7caab
Sadan Iqbal <sadaniqbal@gmail.com>
2020-04-22 14:40:24 -0700

    Completed Part one

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector: "app",
   // templateUrl: "./app.component.html",
-  template: `<div class="bg-success p-2 text-center text-white">
-    This is Sports Store
-  </div>`,
+  template: "<store></store>",
 })
 export class AppComponent {
   title = "IS645Chap7-SuperStore";
